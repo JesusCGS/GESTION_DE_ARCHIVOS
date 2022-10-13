68774e185852714b2a5adb1cd724c904e3b3d660
JesusCGS <abcenu2002@gmail.com>
2022-10-13 14:13:44 +0200

    parte 2
    crear archivo; escribir archivo; agregar archivo; leer archivo

--- ManejoDeArchivos/src/Principal/PPal.java
+++ ManejoDeArchivos/src/Principal/PPal.java
@@ -13,7 +13,29 @@ import manejodearchivos.mane_de_archivos;
  */
 public class PPal {
     public static void main(String[] args) {
+        
         String nombreArchivo = "prueba.txt";
         mane_de_archivos.CREARARCHIVO(nombreArchivo);
+        mane_de_archivos.escribirarchivo(nombreArchivo, "es la hora de programar");//esto soobrescribe el archivo
+        mane_de_archivos.egregararchivo("prueba.txt", "destiny 2");
+        mane_de_archivos.egregararchivo("prueba.txt", "A luis le gustan los furros");
+        mane_de_archivos.leerarchivo("prueba.txt");
+        
+        mane_de_archivos.leerarchivoporpalabras("prueba.txt");
+        
+        
+        
+        
+        
+        
+        
+        
+        /*/jesus PASO 1
+            String nombreArchivo2 = "pruebajesus1.txt";
+            String contenido2 = "hola me llamo jesu";
+            mane_de_archivos.CREARARCHIVO(nombreArchivo2);
+            mane_de_archivos.escribirarchivo(nombreArchivo2, contenido2);
+            
+        */
     }
 }
--- ManejoDeArchivos/src/manejodearchivos/mane_de_archivos.java
+++ ManejoDeArchivos/src/manejodearchivos/mane_de_archivos.java
@@ -7,9 +7,16 @@
  */
 package manejodearchivos;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -26,6 +33,77 @@ public class mane_de_archivos {
         }catch(FileNotFoundException ex){
                ex.printStackTrace(System.out);
         } 
+    }
+    public static void escribirarchivo(String nombre,String contenido){
+        PrintWriter salida = null;
+         File archivo = new File(nombre);
+        try {//se que el fichero exixte
+            salida = new PrintWriter(archivo);
+            salida.println(contenido);
+            System.out.println("se ha credo el archivo!!!");
+        } catch (FileNotFoundException ex) {//el fichero no exixte->excep.
+            ex.printStackTrace(System.out);
+        } finally {//este bloque siempre se ejecuta
+            salida.close();
+        }
+        
+        
+    }
+    
+    //este metodo agrega texto al archivo , no,lo sobreescribe
+    public static void egregararchivo(String nombre,String contenido){
+        PrintWriter salida = null;
+          File archivo = new File(nombre);
+        try {
+             salida = new PrintWriter(new FileWriter(nombre,true));
+             salida.println(contenido);
+             System.out.println("se ha añadido el contenido nuevo");
+        } catch (IOException ex) {
+          
+        } finally {//este bloque siempre se ejecuta
+            salida.close();
+        }
+    }
+    
+    public static void leerarchivo(String nombre){
+        //1.se declara el file
+         BufferedReader entrada;
+         File archivo = new File(nombre);
+        try {
+            //2.creamos el descriptor de lectura del fichero
+             entrada = new BufferedReader(new FileReader(archivo));
+              String lectura = entrada.readLine();
+            //3. ahora recorremos lectura hasta fin de archivo
+            while (lectura!= null){
+                System.out.println("lectura :" +lectura);
+                lectura= entrada.readLine();
+            }
+            entrada.close();
+        } catch (FileNotFoundException ex) {
+           ex.printStackTrace(System.out);
+        }catch (IOException ex){
+             ex.printStackTrace(System.out);
+        } 
+    }
+    
+    
+    public static void leerarchivoporpalabras(String nombre){
+        File archivo = new File(nombre);
+        
+        try {
+            Scanner entrada = new Scanner(archivo);
+            int cont =0;
+            //recorro el fichero hasta fin
+            while(entrada.hasNext()){
+                String palabra = entrada.next();
+                System.out.println(palabra);
+                cont = cont +1;
+            }
+            System.out.println("NUMERO DE PALABRAS DE MI FICHERO = "+cont);
+            
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        }
     }
     
 }
